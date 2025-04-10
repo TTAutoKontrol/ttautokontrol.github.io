9ae1ab28f4dba63193649284dc3f3fd69e059cd7
Aleksandar <alt.dszi@gmail.com>
2025-04-10 13:09:06 +0200

    Add built files to main branch

---
+++ _astro/hoisted.BdZzNzV7.js
@@ -0,0 +1,1 @@
+import"./hoisted.BX5CRztz.js";document.addEventListener("DOMContentLoaded",()=>{const e=new URL(window.location.href);e.searchParams.has("page")&&(e.searchParams.delete("page"),window.history.replaceState({},"",e))});
