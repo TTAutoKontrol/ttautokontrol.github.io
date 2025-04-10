1a57c2c6d6c7200763949014152325d0368e3bc8
Aleksandar <alt.dszi@gmail.com>
2025-04-10 18:10:54 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ttautokontrol.mk',
+  site: 'https://ttautokontrol.github.io',
+  base: '/',
   integrations: [
     tailwind(),
-    sitemap(),
   ],
 }); 
